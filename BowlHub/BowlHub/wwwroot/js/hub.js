251d03bc84c44cc108e41867a6e4e44f49746e1b
Illia <karas.ilya@ukr.net>
2023-09-30 15:59:52 +0200

    Added visitors page, changed some frontend

---
+++ BowlHub/BowlHub/wwwroot/js/hub.js
@@ -0,0 +1,36 @@
+﻿async function getAllPlaces(){
+    const response = await fetch("/getAllPlaces", {
+        method: "GET",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+    })
+
+    if (response.ok === true) {
+        const data = (await response.json()).value
+        const cardsContainer = document.body.querySelector(".cards-container")
+        data.forEach(cardData => {
+            cardsContainer.innerHTML += `
+            <section class="card">
+                <div class="card-header">
+                    <h3>${cardData.name}</h3>
+                </div>
+                <div class="card-body">
+                    <div class="card-field">
+                        <h3 class="card-field-name">City:</h3>
+                        <span class="card-field-content">${cardData.city}</span>
+                    </div>
+                    <div class="card-field">
+                        <h3 class="card-field-name">Address:</h3>
+                        <span class="card-field-content">${cardData.address}</span>
+                    </div>
+                    <div class="card-field">
+                        <h3 class="card-field-name">Phone:</h3>
+                        <span class="card-field-content">${cardData.adminPhone}</span>
+                    </div>
+                </div>
+                <input type="button" class="card-button" value="Select">
+            </section>`
+        })
+    }
+}
+
+getAllPlaces()

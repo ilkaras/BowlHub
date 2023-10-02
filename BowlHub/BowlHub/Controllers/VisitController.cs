1889bbe258abe8c6ded66820f8c4ba46cb46652d
Illia <karas.ilya@ukr.net>
2023-10-03 00:24:43 +0200

    Added Visit controller

---
+++ BowlHub/BowlHub/Controllers/VisitController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BowlHub.BLL.Services.Interfaces;
+using BowlHub.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BowlHub.Controllers;
+
+[ApiController]
+public class VisitController : Controller
+{
+    public VisitController()
+    {
+    }
+    
+    [HttpGet("")]
+    public IActionResult RenderVisit()
+    {
+        return View("../Pages/Visit");
+    }
+}

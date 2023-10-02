c3e6035d93c4488f9447821732b99500e0a03b74
Illia <karas.ilya@ukr.net>
2023-10-03 00:16:22 +0200

    Fully completed mapper

--- BowlHub/BowlHub.BLL/AppMappingProfile.cs
+++ BowlHub/BowlHub.BLL/AppMappingProfile.cs
@@ -31,5 +31,81 @@ public class AppMappingProfile : Profile
                 opt => opt.MapFrom(place => place.Adress))
             .ForMember(place => place.AdminPhone,
                 opt => opt.MapFrom(place => place.AdminPhone));
+        
+                CreateMap<BoardEntity, BoardModel>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.PlaceId,
+                opt => opt.MapFrom(place => place.PlaceId))
+            .ForMember(place => place.ColumnCount,
+                opt => opt.MapFrom(place => place.ColumnCount))
+            .ForMember(place => place.StartTime,
+                opt => opt.MapFrom(place => place.StartTime))
+            .ForMember(place => place.EndTime,
+                opt => opt.MapFrom(place => place.EndTime));
+        
+        CreateMap<BoardModel, BoardEntity>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.PlaceId,
+                opt => opt.MapFrom(place => place.PlaceId))
+            .ForMember(place => place.ColumnCount,
+                opt => opt.MapFrom(place => place.ColumnCount))
+            .ForMember(place => place.StartTime,
+                opt => opt.MapFrom(place => place.StartTime))
+            .ForMember(place => place.EndTime,
+                opt => opt.MapFrom(place => place.EndTime));
+        
+        CreateMap<ReservationModel, ReservationEntity>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.UserId,
+                opt => opt.MapFrom(place => place.UserId))
+            .ForMember(place => place.BoardId,
+                opt => opt.MapFrom(place => place.BoardId))
+            .ForMember(place => place.ColumnNum,
+                opt => opt.MapFrom(place => place.ColumnNum))
+            .ForMember(place => place.FromReservation,
+                opt => opt.MapFrom(place => place.FromReservation))
+            .ForMember(place => place.TillReservation,
+                opt => opt.MapFrom(place => place.TillReservation))
+            .ForMember(place => place.DateReservation,
+                opt => opt.MapFrom(place => place.DateReservation));
+        
+        CreateMap<ReservationEntity, ReservationModel>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.UserId,
+                opt => opt.MapFrom(place => place.UserId))
+            .ForMember(place => place.BoardId,
+                opt => opt.MapFrom(place => place.BoardId))
+            .ForMember(place => place.ColumnNum,
+                opt => opt.MapFrom(place => place.ColumnNum))
+            .ForMember(place => place.FromReservation,
+                opt => opt.MapFrom(place => place.FromReservation))
+            .ForMember(place => place.TillReservation,
+                opt => opt.MapFrom(place => place.TillReservation))
+            .ForMember(place => place.DateReservation,
+                opt => opt.MapFrom(place => place.DateReservation));
+
+        CreateMap<UserEntity, UserModel>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.Email,
+                opt => opt.MapFrom(place => place.Email))
+            .ForMember(place => place.Password,
+                opt => opt.MapFrom(place => place.Password))
+            .ForMember(place => place.Name,
+                opt => opt.MapFrom(place => place.Name));
+        
+        CreateMap<UserModel, UserEntity>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.Email,
+                opt => opt.MapFrom(place => place.Email))
+            .ForMember(place => place.Password,
+                opt => opt.MapFrom(place => place.Password))
+            .ForMember(place => place.Name,
+                opt => opt.MapFrom(place => place.Name));
     }
 }
--- BowlHub/BowlHub/Extensions/AppMapperProfile.cs
+++ BowlHub/BowlHub/Extensions/AppMapperProfile.cs
@@ -31,5 +31,89 @@ public class AppMapperProfile : Profile
                 opt => opt.MapFrom(place => place.Address))
             .ForMember(place => place.AdminPhone,
                 opt => opt.MapFrom(place => place.AdminPhone));
+        
+        CreateMap<BoardDto, BoardModel>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.PlaceId,
+                opt => opt.MapFrom(place => place.PlaceId))
+            .ForMember(place => place.ColumnCount,
+                opt => opt.MapFrom(place => place.ColumnCount))
+            .ForMember(place => place.StartTime,
+                opt => opt.MapFrom(place => place.StartTime))
+            .ForMember(place => place.EndTime,
+                opt => opt.MapFrom(place => place.EndTime));
+        
+        CreateMap<BoardModel, BoardDto>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.PlaceId,
+                opt => opt.MapFrom(place => place.PlaceId))
+            .ForMember(place => place.ColumnCount,
+                opt => opt.MapFrom(place => place.ColumnCount))
+            .ForMember(place => place.StartTime,
+                opt => opt.MapFrom(place => place.StartTime))
+            .ForMember(place => place.EndTime,
+                opt => opt.MapFrom(place => place.EndTime));
+        
+        CreateMap<ReservationModel, ReservationDto>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.UserId,
+                opt => opt.MapFrom(place => place.UserId))
+            .ForMember(place => place.BoardId,
+                opt => opt.MapFrom(place => place.BoardId))
+            .ForMember(place => place.ColumnNum,
+                opt => opt.MapFrom(place => place.ColumnNum))
+            .ForMember(place => place.FromReservation,
+                opt => opt.MapFrom(place => place.FromReservation))
+            .ForMember(place => place.TillReservation,
+                opt => opt.MapFrom(place => place.TillReservation))
+            .ForMember(place => place.DateReservation,
+                opt => opt.MapFrom(place => place.DateReservation));
+        
+        CreateMap<ReservationDto, ReservationModel>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.UserId,
+                opt => opt.MapFrom(place => place.UserId))
+            .ForMember(place => place.BoardId,
+                opt => opt.MapFrom(place => place.BoardId))
+            .ForMember(place => place.ColumnNum,
+                opt => opt.MapFrom(place => place.ColumnNum))
+            .ForMember(place => place.FromReservation,
+                opt => opt.MapFrom(place => place.FromReservation))
+            .ForMember(place => place.TillReservation,
+                opt => opt.MapFrom(place => place.TillReservation))
+            .ForMember(place => place.DateReservation,
+                opt => opt.MapFrom(place => place.DateReservation));
+
+        CreateMap<UserDto, UserModel>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.Email,
+                opt => opt.MapFrom(place => place.Email))
+            .ForMember(place => place.Password,
+                opt => opt.MapFrom(place => place.Password))
+            .ForMember(place => place.Name,
+                opt => opt.MapFrom(place => place.Name));
+        
+        CreateMap<UserModel, UserDto>()
+            .ForMember(place => place.Id,
+                opt => opt.MapFrom(place => place.Id))
+            .ForMember(place => place.Email,
+                opt => opt.MapFrom(place => place.Email))
+            .ForMember(place => place.Password,
+                opt => opt.MapFrom(place => place.Password))
+            .ForMember(place => place.Name,
+                opt => opt.MapFrom(place => place.Name));
+        
+        CreateMap<UserSignupDto, UserModel>()
+            .ForMember(place => place.Email,
+                opt => opt.MapFrom(place => place.Email))
+            .ForMember(place => place.Password,
+                opt => opt.MapFrom(place => place.Password))
+            .ForMember(place => place.Name,
+                opt => opt.MapFrom(place => place.Name));
     }
 }

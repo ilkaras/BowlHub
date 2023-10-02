d9dec35c75819ec5dc1efbb68c958601df5c0210
Illia <karas.ilya@ukr.net>
2023-10-03 00:26:11 +0200

    Added functions in User repository

--- BowlHub/BowlHub.DAL/Repositories/Classes/UserRepository.cs
+++ BowlHub/BowlHub.DAL/Repositories/Classes/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BowlHub.DAL.Entities;
 using BowlHub.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BowlHub.DAL.Repositories.Classes;
 
@@ -8,5 +9,17 @@ public class UserRepository : BaseRepository<UserEntity>, IUserRepository
 {
     public UserRepository(DataBaseContext context) : base(context)
     {
+    }
+
+    public async Task<UserEntity?> GetUserByEmail(string email)
+    {
+        var result = await DbSet.FirstOrDefaultAsync(x => x.Email == email);
+        return result;
+    }
+
+    public async Task<bool> CheckUser(string email, string password)
+    {
+        var result = await DbSet.FirstOrDefaultAsync(x => x.Email == email && x.Password == password);
+        return result != null;
     }
 }

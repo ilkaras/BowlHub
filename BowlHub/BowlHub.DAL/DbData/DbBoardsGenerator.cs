ef4c90c4cc46b502c368d55a1f5c2445220131bc
Illia <karas.ilya@ukr.net>
2023-10-06 01:18:53 +0200

    Small changes

--- BowlHub/BowlHub.DAL/DbData/DbBoardsGenerator.cs
+++ BowlHub/BowlHub.DAL/DbData/DbBoardsGenerator.cs
@@ -12,24 +12,24 @@ public static class DbBoardsGenerator
             Id = new Guid("7f319f6e-85dd-4e23-ac9f-5acd0b2a1b39"),
             PlaceId = new Guid("60dc9058-55d3-4a55-9594-a9684a503c5b"),
             ColumnCount = 5,
-            StartTime = "12:00",
-            EndTime = "17:00",
+            StartTime = 720,
+            EndTime = 1020,
         },
         new BoardEntity
         {
             Id = new Guid("d8b70b7d-59d9-4469-a8a5-84fe2c0f35bb"),
             PlaceId = new Guid("9a946ef3-1d89-4764-9e81-43813bb422c1"),
             ColumnCount = 8,
-            StartTime = "10:00",
-            EndTime = "22:00",
+            StartTime = 600,
+            EndTime = 1320,
         },
         new BoardEntity
         {
             Id = new Guid("a5e6e90f-9d38-4a4d-8e29-138f0764b0f8"),
             PlaceId = new Guid("aaa85140-61ee-4e07-9ba8-32733a38442d"),
             ColumnCount = 14,
-            StartTime = "8:00",
-            EndTime = "20:00",
+            StartTime = 480,
+            EndTime = 1200,
         },
 
     };

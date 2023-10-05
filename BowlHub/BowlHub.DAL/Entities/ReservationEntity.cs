a6870e7066640cfdb603cf218acb866535071334
Illia <karas.ilya@ukr.net>
2023-10-06 01:16:51 +0200

    Added reservation repository,entity,interface

--- BowlHub/BowlHub.DAL/Entities/ReservationEntity.cs
+++ BowlHub/BowlHub.DAL/Entities/ReservationEntity.cs
@@ -4,9 +4,9 @@ public class ReservationEntity : BaseEntity
 {
     public Guid UserId { get; set; }
     public Guid BoardId { get; set; }
-    public Guid ColumnNum { get; set; }
-    public DateTime FromReservation { get; set; }
-    public DateTime TillReservation { get; set; }
+    public int ColumnNum { get; set; }
+    public int FromReservation { get; set; }
+    public int TillReservation { get; set; }
     public DateTime DateReservation { get; set; }
     
     public virtual UserEntity? User { get; set; }
--- BowlHub/BowlHub.DAL/Repositories/Classes/ReservationRepository.cs
+++ BowlHub/BowlHub.DAL/Repositories/Classes/ReservationRepository.cs
@@ -1,11 +1,53 @@
 ﻿using BowlHub.DAL.Entities;
 using BowlHub.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BowlHub.DAL.Repositories.Classes;
 
 public class ReservationRepository : BaseRepository<ReservationEntity>, IReservationRepository
 {
+    private readonly DbSet<BoardEntity> _boardDbSet;
+    
     public ReservationRepository(DataBaseContext context) : base(context)
     {
+        _boardDbSet = context.Set<BoardEntity>();
+    }
+
+    public async Task<Dictionary<string, List<int[]>>> GetTimeInfoByLineId(Guid id, int lineId)
+    {
+        var today = DateTime.Today;
+        var board = await _boardDbSet.FirstOrDefaultAsync(x => x.PlaceId == id);
+        
+        List<int[]> busyTimes = await DbSet
+            .Where(r => r.BoardId == board!.Id && r.ColumnNum == lineId)
+            .OrderBy(r => r.FromReservation)
+            .Select(r => new int[] { r.FromReservation, r.TillReservation }) 
+            .ToListAsync();
+
+        List<int[]> freeTimes = new List<int[]>();
+
+        int startOfDay = board!.StartTime;
+        int endOfDay = board.EndTime;
+        int lastEnd = startOfDay;
+
+        foreach (var busyTime in busyTimes)
+        {
+            if (busyTime[0] > lastEnd)
+            {
+                freeTimes.Add(new int[] { lastEnd, busyTime[0] });
+            }
+            lastEnd = busyTime[1];
+        }
+
+        if (lastEnd < endOfDay)
+        {
+            freeTimes.Add(new int[] { lastEnd, endOfDay });
+        }
+
+        return new Dictionary<string, List<int[]>> 
+        {
+            { "busy", busyTimes },
+            { "free", freeTimes }
+        };
     }
 }

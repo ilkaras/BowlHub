2e5bf237975d659fb4465bcbb67f414f53aa672a
Illia <karas.ilya@ukr.net>
2023-10-06 01:17:13 +0200

    Small changes in types

--- BowlHub/BowlHub/DTOs/ReservationDto.cs
+++ BowlHub/BowlHub/DTOs/ReservationDto.cs
@@ -5,8 +5,8 @@ public class ReservationDto
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
     public Guid BoardId { get; set; }
-    public Guid ColumnNum { get; set; }
-    public DateTime FromReservation { get; set; }
-    public DateTime TillReservation { get; set; }
+    public int ColumnNum { get; set; }
+    public int FromReservation { get; set; }
+    public int TillReservation { get; set; }
     public DateTime DateReservation { get; set; }
 }

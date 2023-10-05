f757dda12ab804f6c0e6a7d371cb758d06c512c6
Illia <karas.ilya@ukr.net>
2023-10-06 01:21:24 +0200

    Added Reservation servise,interface

---
+++ BowlHub/BowlHub.BLL/Services/Classes/ReservationService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BowlHub.BLL.Models;
+using BowlHub.BLL.Services.Interfaces;
+using BowlHub.DAL.Entities;
+using BowlHub.DAL.Repositories.Interfaces;
+
+namespace BowlHub.BLL.Services.Classes;
+
+public class ReservationService : IReservationService
+{
+    private readonly IReservationRepository _reservationRepository;
+    private readonly IMapper _mapper;
+    
+    public ReservationService(IReservationRepository repository, IMapper mapper)
+    {
+        _reservationRepository = repository;
+        _mapper = mapper;
+    }
+    
+    public async Task<Dictionary<string, List<int[]>>> GetTimeInfoByLineId(Guid id, int lineId)
+    {
+        return await _reservationRepository.GetTimeInfoByLineId(id, lineId);
+    }
+
+    public async Task<ReservationModel> AddNewReservation(ReservationModel reservationModel)
+    {
+        var result = await _reservationRepository.AddAsync(_mapper.Map<ReservationEntity>(reservationModel));
+        return _mapper.Map<ReservationModel>(result);
+    }
+}
---
+++ BowlHub/BowlHub.BLL/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,9 @@
+﻿using BowlHub.BLL.Models;
+
+namespace BowlHub.BLL.Services.Interfaces;
+
+public interface IReservationService
+{
+    Task<Dictionary<string, List<int[]>>> GetTimeInfoByLineId(Guid id, int lineId);
+    Task<ReservationModel> AddNewReservation(ReservationModel reservationModel);
+}
